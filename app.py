5259caaeaadd9e55bcbaac6e780ad7346c088778
Codefinity-py <ats@codefinity.py>
2024-02-26 08:43:58 +0200

    Section 3 Flask-Smorest and Schema

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 from flask import Flask
 from dotenv import load_dotenv
+from flask_smorest import Api
+from flask_migrate import Migrate
 
 from db import db
+
 
 load_dotenv()
 
@@ -13,7 +16,12 @@ def create_app():
     app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///db.sqlite3")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["API_TITLE"] = "FC Manager REST API"
+    app.config["API_VERSION"] = "v1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
 
+    api = Api(app)
     db.init_app(app)
 
     with app.app_context():
